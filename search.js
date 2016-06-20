e4beec0e36b148f3873dd729b868fbbe44dafcc4
Jeffery Bennett <meandmybadself@gmail.com>
2016-06-20 17:26:16 -0500

    Adding support for single-result results.

--- search.js
+++ search.js
@@ -3,211 +3,229 @@
  */
 
 // jshint node:true
-'use strict';
+'use strict'
 
 // CONSTANTS
-var HOST = 'http://tmsearch.uspto.gov';
-var HANDLER_URL = HOST + '/bin/gate.exe';
-var LOGIN = HANDLER_URL + '?f=login&p_lang=english&p_d=trmk';
-var SERIAL_ROW_NUMBER = 2;
-var IMAGE_HANDLER_URL = HOST + '/ImageAgent/ImageAgentProxy?getImage={serialNumber}';
+var HOST = 'http://tmsearch.uspto.gov'
+var HANDLER_URL = HOST + '/bin/gate.exe'
+var LOGIN = HANDLER_URL + '?f=login&p_lang=english&p_d=trmk'
+var SERIAL_ROW_NUMBER = 2
+var IMAGE_HANDLER_URL = HOST + '/ImageAgent/ImageAgentProxy?getImage={serialNumber}'
 
 var selectors = {
-    documentLink: 'a[href*="f=doc"]',
-    logoutButton: 'input[value="Logout"]',
-    resultsTable: 'table:contains(Filing Date)',
-    searchForm: 'form[name=search_text]',
-    searchPageLink: 'a:contains("Free Form")',
-    titleContainingError: 'title:contains(Error)'
-};
+  documentLink: 'a[href*="f=doc"]',
+  logoutButton: 'input[value="Logout"]',
+  resultsTable: 'table:contains(Filing Date)',
+  searchForm: 'form[name=search_text]',
+  searchPageLink: 'a:contains("Free Form")',
+  titleContainingError: 'title:contains(Error)'
+}
 
-var MAX_PARALLEL_DOCUMENTS = 1;  // TESS sometimes throws errors when same session requests multiple pages at once
-//var MAX_PARALLEL_IMAGES = 10;
+var MAX_PARALLEL_DOCUMENTS = 1 // TESS sometimes throws errors when same session requests multiple pages at once
+// var MAX_PARALLEL_IMAGES = 10
 
-var rp = require('request-promise');
-var Promise = require('bluebird').Promise;
-var cheerio = require('cheerio');
-var extend = require('extend');
+var rp = require('request-promise')
+var Promise = require('bluebird').Promise
+var cheerio = require('cheerio')
+var extend = require('extend')
+var requestsCompleted
+var documentData
+var progressHandler
 
-var jar = rp.jar();
+var jar = rp.jar()
 
 rp = rp.defaults({
-    jar: jar,
-    //proxy: 'http://localhost:8888',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0 (compatible; TESS-GIF)'
-    }
-});
+  jar: jar,
+//  proxy: 'http://localhost:8888',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0 (compatible; TESS-GIF)'
+  }
+})
 
 /**
  * @param {Cheerio} $form - form element to serialize
  * @returns {object} - object mapping field names to values
  */
-function serializeForm($form) {
-    return $form.serializeArray().reduce(function (o, v) {
-        o[v.name] = v.value;
-        return o;
-    }, {});
+function serializeForm ($form) {
+  return $form.serializeArray().reduce(function (o, v) {
+    o[v.name] = v.value
+    return o
+  }, {})
 }
 
 /**
  * @param {string} message - user-facing error message
  * @param {object|string} [details] - any extra error details for debug purposes
  */
-function TessError(message, details) {
-    this.name = 'TessError';
-    this.message = message;
-    this.details = details;
+function TessError (message, details) {
+  this.name = 'TessError'
+  this.message = message
+  this.details = details
 }
-TessError.prototype = Object.create(Error.prototype);
-TessError.prototype.constructor = TessError;
-
+TessError.prototype = Object.create(Error.prototype)
+TessError.prototype.constructor = TessError
 
 /**
  * @param {string} message - user-facing progress message
  * @param {object|string} [details] - details for debug purposes
  */
-function TessProgress(message, details) {
-    this.message = message;
-    this.details = details;
+function TessProgress (message, details) {
+  this.message = message
+  this.details = details
 }
-
 
 /**
  * @param {string} message - user-facing progress message
  * @param {number} fraction - fraction of the current task this progress event marks completed
  * @param {document} object - latest loaded document.
  */
-function TessFractionalProgress(message, fraction, document) {
-    this.message = message;
-    this.fraction = fraction;
+function TessFractionalProgress (message, fraction, document) {
+  this.message = message
+  this.fraction = fraction
 
-    // Don't need this in the payload.  Also, don't affect referenced variable.
-    let docCopy = extend({}, document)
-    docCopy._html = null;
+  // Don't need this in the payload.  Also, don't affect referenced variable.
+  let docCopy = extend({}, document)
+  docCopy._html = null
 
-    this.document = docCopy;
+  this.document = docCopy
 }
+
+function announceProgress (progress, total, newDocument) {
+  var message = 'Retrieving ' + total + ' documents',
+    fraction = (progress > 0) ? progress / total : 0
+  progressHandler(new TessFractionalProgress(message, fraction, newDocument))
+}
+
+function getPromiseForDocument (doc) {
+  return rp.get(doc.docUrl)
+    .then(function (document) {
+      var $ = cheerio.load(document)
+      requestsCompleted++
+
+      doc.full = {}
+      $(selectors.resultsTable).find('tr').each(function (i, el) {
+        var fieldName = $(el).find('td:first-of-type').text().trim(),
+          fieldValue = $(el).find('td:last-of-type').text().trim()
+        doc.full[fieldName] = fieldValue
+      })
+      doc.imageUrl = HOST + $('img[src*="/ImageAgent/"]').attr('src')
+      doc._html = $('body').html()
+
+      announceProgress(requestsCompleted, documentData.length, doc)
+      return doc
+    })
+}
+
+function getSession () {
+  progressHandler(new TessProgress('Logging in to TESS...'))
+
+  return rp(LOGIN)
+    .catch(function (e) {
+      throw new TessError('Encountered error logging in to TESS', e)
+    })
+    .disposer(function (pageData) {
+      var $ = cheerio.load(pageData),
+        $logoutForm = $(selectors.logoutButton).closest('form')
+      if (!$logoutForm.length) {
+        return new TessError('Failed to find TESS logout form!')
+      }
+
+      progressHandler(new TessProgress('Logging out of TESS...'))
+
+      return rp.post({
+        uri: HANDLER_URL,
+        form: serializeForm($logoutForm)
+      }).catch(function () {
+        return new TessError('Encountered an error disposing TESS session!')
+      })
+    })
+}
+
+
 
 
 /**
  * @param {string} searchTerm - TESS free-form query
  * @param {function} [progressHandler] - optional handler for progress-related events
  */
-function search(searchTerm, progressHandler) {
+function search (searchTerm, progressHandlerFn) {
+  /**
+   * Gets a TESS session, returning a Disposer for session cleanup
+   * @returns Bluebird.Promise.prototype.disposer
+   */
 
-    /**
-     * Gets a TESS session, returning a Disposer for session cleanup
-     * @returns Bluebird.Promise.prototype.disposer
-     */
-    function getSession() {
+   progressHandler = progressHandlerFn
 
-        progressHandler(new TessProgress('Logging in to TESS...'));
+  return Promise.using(getSession(), function (entryPageHtml) {
+    var $ = cheerio.load(entryPageHtml),
 
-        return rp(LOGIN)
-            .catch(function (e) {
-                throw new TessError('Encountered error logging in to TESS', e);
-            })
-            .disposer(function (pageData) {
-                var $ = cheerio.load(pageData),
-                    $logoutForm = $(selectors.logoutButton).closest('form');
-                if (!$logoutForm.length) {
-                    return new TessError('Failed to find TESS logout form!');
-                }
-
-                progressHandler(new TessProgress('Logging out of TESS...'));
-
-                return rp.post({
-                    uri: HANDLER_URL,
-                    form: serializeForm($logoutForm)
-                }).catch(function () {
-                    return new TessError('Encountered an error disposing TESS session!');
-                });
-            });
+      // Get a link to the search page (which includes session token)
+      link = $(selectors.searchPageLink)
+    if (!link.length) {
+      throw new TessError('Failed to find link to search page!', entryPageHtml)
     }
 
-    return Promise.using(getSession(), function (entryPageHtml) {
-        var $ = cheerio.load(entryPageHtml),
+    // Load the search page
+    return rp(HOST + link.attr('href'), { resolveWithFullResponse: true })
+      .then(function (response) {
+        // Search
+        var $ = cheerio.load(response.body),
+          formData = serializeForm($(selectors.searchForm))
+        formData.p_s_ALL = searchTerm
+        formData.a_search = 'Submit Query'
+        formData.p_L = 500 // 500 results/page
+        // jar.setCookie(rp.cookie('queryCookie=' + encodeURIComponent(formData.p_s_ALL)), HOST)
 
-        // Get a link to the search page (which includes session token)
-        link = $(selectors.searchPageLink);
-        if (!link.length) {
-            throw new TessError('Failed to find link to search page!', entryPageHtml);
+        progressHandler(new TessProgress('Querying ' + searchTerm))
+
+        return rp.post({
+          uri: HANDLER_URL,
+          form: formData,
+          resolveWithFullResponse: true,
+          headers: {
+            Referer: response.request.href
+          }
+        })
+      })
+      .then(function (response) {
+        var $ = cheerio.load(response.body)
+        var titleContainingError = $(selectors.titleContainingError)
+        requestsCompleted = 0
+
+        if (titleContainingError.length) {
+          throw new TessError('Encountered an error querying TESS', $('body').text())
         }
 
-        // Load the search page
-        return rp(HOST + link.attr('href'), { resolveWithFullResponse: true })
-            .then(function (response) {
-                // Search
-                var $ = cheerio.load(response.body),
-                    formData = serializeForm($(selectors.searchForm));
-                formData.p_s_ALL = searchTerm;
-                formData.a_search = 'Submit Query';
-                formData.p_L = 500;  // 500 results/page
-                //jar.setCookie(rp.cookie('queryCookie=' + encodeURIComponent(formData.p_s_ALL)), HOST);
+        progressHandler(new TessProgress('Got search response', response.body))
 
-                progressHandler(new TessProgress('Querying ' + searchTerm));
+        documentData = $('td:nth-of-type(' + SERIAL_ROW_NUMBER + ') ' + selectors.documentLink).toArray().map(function (el) {
+          return {
+            docUrl: HOST + $(el).attr('href')
+          }
+        }) // end map
 
-                return rp.post({
-                    uri: HANDLER_URL,
-                    form: formData,
-                    resolveWithFullResponse: true,
-                    headers: {
-                        Referer: response.request.href
-                    }
-                });
-            })
-            .then(function (response) {
-                var $ = cheerio.load(response.body),
-                    titleContainingError = $(selectors.titleContainingError),
-                    requestsCompleted = 0,
-                    documentData;
 
-                if (titleContainingError.length) {
-                    throw new TessError('Encountered an error querying TESS', $('body').text());
-                }
+        return Promise.map(documentData, getPromiseForDocument, {
+          concurrency: MAX_PARALLEL_DOCUMENTS
+        })
+      })
+      .catch((e) => {
+        // This could be an instance where only one item was returned, and the system is trying to 302 to a non-existant URL.
+        if (e.statusCode === 302) {
+          let redirUrl = e.response.headers.location
 
-                progressHandler(new TessProgress('Got search response', response.body));
+          documentData = [{
+            docUrl: redirUrl
+          }]
 
-                documentData = $('td:nth-of-type(' + SERIAL_ROW_NUMBER + ') ' + selectors.documentLink).toArray().map(function (el) {
+          return Promise.map(documentData, getPromiseForDocument, {
+            concurrency: MAX_PARALLEL_DOCUMENTS
+          })
 
-                    return {
-                        wordMark: $(el).parent().next().next().text(),
-                        imageUrl: IMAGE_HANDLER_URL.replace('{serialNumber}', $(el).html()),
-                        docUrl: HOST + $(el).attr('href')
-                    };
-                });
-
-                function announceProgress(progress, total, newDocument) {
-                    var message = 'Retrieving ' + total + ' documents',
-                        fraction = (progress > 0) ? progress / total : 0;
-                    progressHandler(new TessFractionalProgress(message, fraction, newDocument));
-                }
-
-                function getPromiseForDocument(doc) {
-                    return rp.get(doc.docUrl)
-                        .then(function (document) {
-                            var $ = cheerio.load(document);
-                            requestsCompleted++;
-
-                            doc.full = {};
-                            $(selectors.resultsTable).find('tr').each(function (i, el) {
-                                var fieldName = $(el).find('td:first-of-type').text().trim(),
-                                    fieldValue = $(el).find('td:last-of-type').text().trim();
-                                doc.full[fieldName] = fieldValue;
-                            });
-                            doc._html = $('body').html();
-                            announceProgress(requestsCompleted, documentData.length, doc);
-                            return doc;
-                        });
-                }
-
-                return Promise.map(documentData, getPromiseForDocument, {
-                    concurrency: MAX_PARALLEL_DOCUMENTS
-                });
-
-            });
-    });
+          //getPromiseForDocument(documentData)
+        }
+      })
+  })
 }
 
-module.exports = search;
+module.exports = search
